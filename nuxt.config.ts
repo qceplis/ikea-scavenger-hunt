06439e2bfea91fe43f727d78609000c4a83b69df
qceplis <qceplis@icloud.com>
2025-03-18 21:25:40 -0600

    add supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module', '@pinia/nuxt']
+  modules: [
+    'vuetify-nuxt-module',
+    '@pinia/nuxt',
+    '@nuxtjs/supabase'
+  ]
 })
