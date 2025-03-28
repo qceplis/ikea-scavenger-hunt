dd8e364bc4ca2aeee6a1b3276262c3317d492aeb
qceplis <qceplis@icloud.com>
2025-03-28 00:19:26 -0600

    add all challenges

--- utils/challengeData.js
+++ utils/challengeData.js
@@ -4,8 +4,43 @@ export const challengeData = [
     name: "Uniquely IKEA",
     challenges: [
       {
-        id: 1,
-        title: "Get cozy with Blahaj",
+        id: 'blahaj',
+        title: "Get cozy with BLÅHAJ",
+        points: 1,
+      },
+      {
+        id: 'lomsk',
+        title: "Read a Swedish book in a LÖMSK",
+        points: 1,
+      },
+      {
+        id: 'pruta',
+        title: "Stack PRUTA",
+        points: 1,
+      },
+      {
+        id: 'candy',
+        title: "Eat some LÖRDAGSGODIS",
+        points: 1,
+      },
+      {
+        id: 'lingonberry',
+        title: "Most amount of different lingonberry flavoured items",
+        points: 1,
+      },
+      {
+        id: 'ikeaBag',
+        title: "Best use of an IKEA bag",
+        points: 1,
+      },
+      {
+        id: 'spellIkea',
+        title: "Spell out “IKEA” using IKEA products",
+        points: 1,
+      },
+      {
+        id: 'flag',
+        title: "Rereate the Swedish Flag",
         points: 1,
       },
     ]
@@ -15,15 +50,284 @@ export const challengeData = [
     name: "Items",
     challenges: [
       {
-        id: 1,
-        title: "Find a Rainbow",
+        id: 'rainbow',
+        title: "Something with a rainbow",
         points: 1,
       },
       {
-        id: 2,
-        title: "Create a Swedish Flag",
+        id: 'funnyName',
+        title: "Funniest name of product",
+        points: 1,
+      },
+      {
+        id: 'longName',
+        title: "Longest name of product",
+        points: 1,
+      },
+      {
+        id: 'sentence',
+        title: "Best use of IKEA product name in a sentence",
+        points: 1,
+      },
+      {
+        id: 'notAPrice',
+        title: "Something with a number - not a price",
+        points: 1,
+      },
+      {
+        id: 'lowestPrice',
+        title: "Lowest priced item you can find",
+        points: 1,
+      },
+      {
+        id: 'highestPrice',
+        title: "Highest priced item you can find",
+        points: 1,
+      },
+      {
+        id: 'confusingItem',
+        title: "Most confusing item",
+        points: 1,
+      },
+      {
+        id: 'uglyItem',
+        title: "Ugliest item",
+        points: 1,
+      },
+      {
+        id: 'hat',
+        title: "Coolest hat (item balanced on head)",
+        points: 1,
+      },
+      {
+        id: 'mostSame',
+        title: "Most amount of the same item held by one person",
         points: 1,
       },
     ]
   },
+  {
+    id: 3,
+    name: "Scenes",
+    challenges: [
+      {
+        id: 'shinji',
+        title: "Recreate “Shinji in a chair” with the most unique chair",
+        points: 1,
+      },
+      {
+        id: 'fairyTale',
+        title: "Recreate a fairy tale",
+        points: 1,
+      },
+      {
+        id: 'superhero',
+        title: "The most awesome superhero ever",
+        points: 1,
+      },
+      {
+        id: 'airGuitar',
+        title: "An epic air guitar concert",
+        points: 1,
+      },
+      {
+        id: 'soapOpera',
+        title: "The most dramatic soap opera finale",
+        points: 1,
+      },
+      {
+        id: 'creativeScene',
+        title: "Most creative scene",
+        points: 1,
+      },
+    ]
+  },
+  {
+    id: 4,
+    name: "Team Photos",
+    challenges: [
+      {
+        id: 'weAllFit',
+        title: "“I can't believe we all fit in here!”",
+        points: 1,
+      },
+      {
+        id: 'hiding',
+        title: "“I can see you all hiding!”",
+        points: 1,
+      },
+      {
+        id: 'reflection',
+        title: "Most unique reflection of team (not in a mirror)",
+        points: 1,
+      },
+      {
+        id: 'lightbulb',
+        title: "How many people does it take to change a light bulb?",
+        points: 1,
+      },
+      {
+        id: 'familyPhoto',
+        title: "Awkward family photo",
+        points: 1,
+      },
+    ]
+  },
+  {
+    id: 5,
+    name: "Tell a story!",
+    challenges: [
+      {
+        id: 'storyPhoto1',
+        title: "Photo 1 - Setup",
+        points: 2,
+      },
+      {
+        id: 'storyPhoto2',
+        title: "Photo 2 - Action",
+        points: 2,
+      },
+      {
+        id: 'storyPhoto3',
+        title: "Photo 3 - Resolution/Twist",
+        points: 2,
+      },
+    ]
+  },
+  {
+    id: 6,
+    name: "Products beginning with letters of the alphabet",
+    challenges: [
+      {
+        id: 'A',
+        title: "A",
+        points: 0.25,
+      },
+      {
+        id: 'B',
+        title: "B",
+        points: 0.25,
+      },
+      {
+        id: 'C',
+        title: "C",
+        points: 0.25,
+      },
+      {
+        id: 'D',
+        title: "D",
+        points: 0.25,
+      },
+      {
+        id: 'E',
+        title: "E",
+        points: 0.25,
+      },
+      {
+        id: 'F',
+        title: "F",
+        points: 0.25,
+      },
+      {
+        id: 'G',
+        title: "G",
+        points: 0.25,
+      },
+      {
+        id: 'H',
+        title: "H",
+        points: 0.25,
+      },
+      {
+        id: 'I',
+        title: "I",
+        points: 0.25,
+      },
+      {
+        id: 'J',
+        title: "J",
+        points: 0.25,
+      },
+      {
+        id: 'K',
+        title: "K",
+        points: 0.25,
+      },
+      {
+        id: 'L',
+        title: "L",
+        points: 0.25,
+      },
+      {
+        id: 'M',
+        title: "M",
+        points: 0.25,
+      },
+      {
+        id: 'N',
+        title: "N",
+        points: 0.25,
+      },
+      {
+        id: 'O',
+        title: "O",
+        points: 0.25,
+      },
+      {
+        id: 'P',
+        title: "P",
+        points: 0.25,
+      },
+      {
+        id: 'Q',
+        title: "Q",
+        points: 0.25,
+      },
+      {
+        id: 'R',
+        title: "R",
+        points: 0.25,
+      },
+      {
+        id: 'S',
+        title: "S",
+        points: 0.25,
+      },
+      {
+        id: 'T',
+        title: "T",
+        points: 0.25,
+      },
+      {
+        id: 'U',
+        title: "U",
+        points: 0.25,
+      },
+      {
+        id: 'V',
+        title: "V",
+        points: 0.25,
+      },
+      {
+        id: 'W',
+        title: "W",
+        points: 0.25,
+      },
+      {
+        id: 'X',
+        title: "X",
+        points: 0.25,
+      },
+      {
+        id: 'Y',
+        title: "Y",
+        points: 0.25,
+      },
+      {
+        id: 'Z',
+        title: "Z",
+        points: 0.25,
+      },
+    ]
+  }
 ]
