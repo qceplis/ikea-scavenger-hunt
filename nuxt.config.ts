c7ff4df4a96bcde82ac41242bad6505d0eb13029
qceplis <qceplis@icloud.com>
2025-03-29 11:19:13 -0600

    env vars

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  runtimeConfig: {
+    app: {
+      supabaseKey: process.env.NUXT_SUPABASE_KEY,
+    },
+    public: {
+      supabaseURL: process.env.NUXT_PUBLIC_SUPABASE_URL,
+    }
+  },
   modules: [
     'vuetify-nuxt-module',
     '@pinia/nuxt',
